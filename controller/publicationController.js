173a4e30fb538f1bcd91944cea4b9b4b831cc6dd
mfuentealba@larrainvial.com <http://quilimari.larrainvial.com>
2018-04-17 09:56:26 -0300

    sdfsdfsdf

--- controller/publicationController.js
+++ controller/publicationController.js
@@ -72,6 +72,42 @@ function getPublications(req, res){
 
 }
 
+function getPublicationsUser(req, res){
+    var page = 1;
+    if(req.params.page){
+        page = req.params.page;
+    }
+
+    var user_id = req.user.sub;
+
+    if(req.params.userId){
+        user_id = req.params.userId;
+    }
+
+
+    var itemsPerPage = 4;
+
+    
+
+    Publication.find({user: user_id}).sort('-created_at').populate('user').paginate(page, itemsPerPage, (err, publications, total) => {
+        if(err) return res.status(500).send({message: 'Error devolver publicaciones'});
+        if(!publications){
+            return res.status(404).send({message: 'No hay publicaciones'});
+        }
+        return res.status(200).send({
+            totalItems: total,
+            pages: Math.ceil(total / itemsPerPage),
+            page: page,
+            items_per_page: itemsPerPage,
+            publications: publications
+        })
+    })
+    
+
+}
+
+
+
 function getPublication(req, res){
     var publicationId = req.params.id;
 
@@ -176,6 +212,7 @@ module.exports = {
     
     savePublication,
     getPublications,
+    getPublicationsUser,
     getPublication,
     deletePublication,
     uploadImage,
